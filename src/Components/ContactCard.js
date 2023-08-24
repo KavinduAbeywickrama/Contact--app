728af1869cbc9a0c6a69c88f6688e2ef3ae11879
KavinduAbeywickrama <agckavindu@gmail.com>
2023-08-24 17:37:31 +0530

    feat: updated contactDetail comonent

--- src/Components/ContactCard.js
+++ src/Components/ContactCard.js
@@ -5,12 +5,19 @@ import {Link} from "react-router-dom";
 
 const ContactCard = (props) => {
     const {id,name,email} = props.contact;
-    console.log(name);
+    const contact =  props.contact;
+    const stateObject = {
+        id,
+        name,
+        email,
+      };
+      const stateData = JSON.stringify(stateObject);
+
     return (
         <div className = "item" style ={{display:"flex"}}>
             <img className="ui avatar image" src={user} alt = "user" style ={{float:"left"}} />
                 <div className="content">
-                   <Link to={`/contact/${id}`}>
+                    <Link to={{ pathname: `/contact/${encodeURIComponent(stateData)}`}}>
                         <div className="header">{name}</div>
                         <div>{email}</div>
                     </Link>
--- src/Components/ContactDetail.js
+++ src/Components/ContactDetail.js
@@ -1,20 +1,42 @@
 import React from "react";
 import user from "../images/kavi.jpg";
-import {Link} from "react-router-dom";
+import { Link, useParams, useLocation } from "react-router-dom"; 
+
 
 const ContactDetail = (props) => {
+        // Access URL parameter
+        const location = useLocation();
+        const { pathname } = location;
+        // Extract and decode the data parameter manually from the pathname
+        const dataParameter = pathname.split('/').pop();
+        const decodedData = decodeURIComponent(dataParameter);
+        console.log(decodedData)
+        // Decode the URL parameter (JSON string) and parse it into an object
+        let stateObject = {};
+
+        try {
+            stateObject = JSON.parse(decodedData);
+            console.log('json',stateObject)
+        } catch (error) {
+            console.error('Error parsing JSON:', error);
+  }
+    //console.log(stateObject)
     return (        
         <div className = "main">
             <div className = "ui card centered">
-                <div className = "image">
-                    console.log("props",props);
+                <div className = "image">                   
                     <img src = {user} alt = "user" />
                 </div>
                 <div className = "content">
-                    <div className = "header">Kavi</div>
-                    <div className = "description">Kavi.com</div>  
-                </div>
+                    <div className = "header">{stateObject.name}</div>
+                    <div className = "description">{stateObject.email}</div>  
+                </div>                   
             </div>
+            <div className = "center-div">
+                    <Link to = "/">
+                        <button className = "ui button blue center">Back to Contact List</button>
+                    </Link>
+            </div>                 
         </div>        
     );
 };
